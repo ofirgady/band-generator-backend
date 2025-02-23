d32b9dac5d08dfc25a43f382a337ab4dbbe47584
ofirgady <OFirgady@gmail.com>
2025-02-23 22:47:11 +0200

    add CORS middleware to allow requests from specific origins

--- server.js
+++ server.js
@@ -5,9 +5,14 @@ import express from 'express';
 // app setup
 const app = express();
 
+
 // middleware
 app.use(express.json());
 app.use(express.static('public'));
+app.use(cors({
+    origin: ['http://localhost:5173',  'http://127.0.0.1:5173'],
+    credentials: true 
+}));
 
 // routes
 app.post('/openai/meta', generateMeta);
